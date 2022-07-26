80bbb487f8ca90824bc1dda8baa079c8c0488cdd
Hao <61809905+sunsky426@users.noreply.github.com>
2022-07-26 14:38:19 -0400

    Update Constants.py
    amongus

--- Constants.py
+++ Constants.py
@@ -9,7 +9,7 @@ COLUMNS = 6
 BORDER = 150  # Number of pixels to offset grid to the top-left side
 CELL_DIMENSIONS = (100, 100)  # Number of pixels (x,y) for each cell
 SELF_PLAY = True  # whether or not a human will be playing
-KILL_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'kill_sound.wav'))
+KILL_SOUND = pygame.mixer.Sound(os.path.join('Assets', 'Kill sound.mp3'))
 
 class Action(enum.Enum):
     move = 1
