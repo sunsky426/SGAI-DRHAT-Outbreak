530883dd38f360c092a560b870a1be83542697e2
Hao <61809905+sunsky426@users.noreply.github.com>
2022-07-27 14:07:19 -0400

    bugfix

--- Board.py
+++ Board.py
@@ -410,7 +410,7 @@ class Board:
         self.States[target_idx].person = None
         KILL_SOUND.play()
 
-        self.outrage += 50 * (1 - smoothstep(0, 100, self.anxiety))
+        self.outrage += 0.5 * (100 - self.anxiety)
 
         return [True, target_idx]
 
