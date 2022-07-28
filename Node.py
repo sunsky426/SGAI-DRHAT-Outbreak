9b95edc0e8db0d5a5e24d3389ecf897efa1e83b2
rtsej <100522180+rtsej@users.noreply.github.com>
2022-07-28 16:33:57 -0400

    implements game_result
    This might be a little wonky, it will only work if self.state is passed as Gameboard.States

--- Node.py
+++ Node.py
@@ -16,4 +16,19 @@ class Node:
         self.untried_actions = self.untried_actions()
         return
 
-
+def game_result(self):
+    """
+    Should return a reward value based on the result of the game
+    Either a positive value when humans win, based on how many people remain, or a negative value when zombies win
+    """
+    reward = 0
+    if is_game_over():
+        #checks if the game has ended before returning a reward value other than 0
+        for s in state:
+            #this code has been written based on the assumption that that the state will be defined as Gameboard.states
+            if s.person == True and s.person.isZombie == False:
+                reward += 1
+            else:
+                #returns a basic value of -10 whenever the zombies win, can be changed later
+                reward = -10
+    return reward
